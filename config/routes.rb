6af8fb0086f42e71499c2a9be9801efcad08892a
Omar Torres <omar.torres@plated.com>
2018-06-09 14:07:06 -0400

    add users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#root'
+
+  # The 'defaults: { format: :json }' option tells the controller to first look for a .json.jbuilder view rather than an html.erb view.
+  namespace :api, defaults: {format: :json} do 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
