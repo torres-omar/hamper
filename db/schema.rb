ae0c40d676bad0bef0d115721893ca077adfda6e
Omar Torres <omar.torres@plated.com>
2018-06-10 08:18:26 -0400

    add models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_09_190039) do
+ActiveRecord::Schema.define(version: 2018_06_10_121247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,12 +90,11 @@ ActiveRecord::Schema.define(version: 2018_06_09_190039) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "email", null: false
-    t.string "admin_first_name", null: false
-    t.string "admin_last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
